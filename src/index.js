a33874f8062f8d0b0ee852dcaf4fca13dd2e28f0
Pat Beecher <beecherp@gmail.com>
2017-12-24 18:48:44 -0600

    copying

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const API_KEY = "AIzaSyBwx7F6BF8qp8D4-9EArt6GGccnWmhNHLk";
+
+
+const App = () => {
+  return (
+  <div> 
+    <SearchBar />
+  </div>
+  )
+};
+
+//5 component types
+
+ReactDOM.render(<App />, document.querySelector('.container'));
